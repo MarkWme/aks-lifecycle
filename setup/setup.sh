204c0a7edc11bbf7b85296d16de1ae75b1c2ce27
Mark Whitby <m@markw.me>
2021-09-28 19:31:48 +0100

    Adding setup script

---
+++ setup/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# Setup pre-requisites
+#
+
+randomSuffix=$(cat /dev/urandom | tr -dc '[:lower:]' | fold -w ${1:-5} | head -n 1)
+name=aks-${randomSuffix}
+subscriptionId=$(az account list --query '[?isDefault==`true`].id' --output tsv)
+#
+# Create a resource group for the deployment
+#
+resourceGroupId=$(az group create --name ${name}-rg --location westeurope --query id --output tsv)
+
+#
+# Create a service principal for GitHub Actions to use
+# to access an Azure subscription
+#
+azureCredentials=$(az ad sp create-for-rbac --name ${name}-spn --role contributor --scopes $resourceGroupId --sdk-auth)
+
+gh secret set AZURE_CREDENTIALS -r aks-lifecycle -b $azureCredentials 
+gh secret set AZURE_RG -r aks-lifecycle -b $resourceGroupId
+gh secret set AZURE_SUBSCRIPTION -r aks-lifecycle -b $subscriptionId
